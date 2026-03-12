6472b1a55c20ebd0f98033ae406ad758ad6459c8
Karsten Weng <karsten@weng.eu>
2026-03-12 15:22:26 +0100

    update footer: implement icons

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,25 +1,36 @@
 import { TextLink } from "@/components/ui/text-link";
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconBrandInstagram,
+} from "@tabler/icons-react";
 
 export default function Footer() {
   return (
     <footer className="border-t bg-mauve-100">
-      <div className="flex text-xs gap-4 p-2">
+      <div className="flex items-center justify-evenly whitespace-nowrap p-2 text-xs">
         <span>
-          © {new Date().getFullYear()} <span>STREET LENS</span>
+          © {new Date().getFullYear()}{" "}
+          <TextLink href="https://weng.eu/" target="_blank">
+            WENG.EU
+          </TextLink>
         </span>
-
+        <TextLink href="/impressum">Impressum</TextLink>
         <TextLink
           href="https://github.com/uxbyweng/street-lens"
           target="_blank"
         >
-          GitHub
+          <IconBrandGithub size={18} stroke={1.8} />
         </TextLink>
-
-        <TextLink href="https://weng.eu/" target="_blank">
-          WENG.EU
+        <TextLink href="https://www.linkedin.com/in/kweng/" target="_blank">
+          <IconBrandLinkedin size={18} stroke={1.8} />
         </TextLink>
-
-        <TextLink href="/impressum">Impressum</TextLink>
+        <TextLink
+          href="https://www.instagram.com/blnstreetview/"
+          target="_blank"
+        >
+          <IconBrandInstagram size={18} stroke={1.8} />
+        </TextLink>
       </div>
     </footer>
   );
